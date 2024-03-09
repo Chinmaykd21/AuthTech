ae6653387b3f9c367a78dd55a1036a28bb39f4d9
chinmaykd21 <chinmaykd21@gmail.com>
2024-03-09 18:24:50 -0500

    sign in button and minor styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import SignInButton from "@/components/auth/SignInButton";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center gap-6">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold drop-shadow-md"> 🔐 Login </h1>
+      </div>
+      <SignInButton>
+        <Button size="lg" variant="secondary">
+          Sign In
+        </Button>
+      </SignInButton>
+    </main>
+  );
 }
