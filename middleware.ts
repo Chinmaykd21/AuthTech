f175970ffc8026de0eb65492573da48cbcd73dcc
chinmaykd21 <chinmaykd21@gmail.com>
2024-03-10 20:29:47 -0400

    middleware

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,43 @@ import NextAuth from "next-auth";
 
 const { auth } = NextAuth(authConfig);
 
+import {
+  DEFAULT_LOGIN_REDRIECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "./routes";
+
 export default auth((req) => {
   const isLoggedIn = !!req.auth;
-  console.log(`ROUTE: ${req.nextUrl.pathname}\nIS LOGGEDIN: ${isLoggedIn}`);
+  const { nextUrl } = req;
+
+  const pathName = nextUrl?.pathname;
+
+  const isApiAuthRoute = pathName?.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes?.includes(pathName);
+  const isAuthRoute = authRoutes?.includes(pathName);
+
+  // Order matters of the if conditions
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    // TODO: Improve this logic to convert to single line code
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDRIECT, nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return;
 });
 
-// Optionally, don't invoke Middleware on some paths
-// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 // NOTE: Matcher changed from this URL https://clerk.com/docs/quickstarts/nextjs?utm_source=sponsorship&utm_medium=youtube&utm_campaign=code-with-antonio&utm_content=12-31-2023
 export const config = {
   // Every URL will invoke middleware except certain URLs that don't match this pattern
---
+++ routes.ts
@@ -0,0 +1,22 @@
+/**
+ * An array of routes that will be accessible to the user who is not logged in.
+ * These routes do not need an authentication
+ */
+export const publicRoutes = ["/"];
+
+/**
+ * An array of routes that will not be accessible to the user who is not logged in.
+ * These routes do need an authentication.
+ */
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ * The prefix for API authentication routes.
+ * Routes that start with this prefix are used for API authentcation purpose
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after logging in
+ */
+export const DEFAULT_LOGIN_REDRIECT = "/settings";
