9fd2968a0fe0acf38d4a0df709ebdc5b7234bfa5
chinmaykd21 <chinmaykd21@gmail.com>
2024-03-10 21:37:00 -0400

    Login functionality

--- actions/login.ts
+++ actions/login.ts
@@ -2,17 +2,42 @@
 
 import * as z from "zod";
 import { LoginSchema } from "@/schemas";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDRIECT } from "@/routes";
+import { AuthError } from "next-auth";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
 
-  if (!validatedFields.success) {
+  if (!validatedFields?.success) {
     return {
       error: "Invalid Fields!",
     };
   }
 
-  return {
-    success: "Email sent!",
-  };
+  const { email, password } = validatedFields?.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDRIECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return {
+            error: "Invalid Credentials!",
+          };
+        default:
+          return {
+            error: "Something Went Wrong!",
+          };
+      }
+    }
+
+    // Required in next JS. TODO: Read and update comment accordingly
+    throw error;
+  }
 };
--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,33 @@
-import GitHub from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 
 import type { NextAuthConfig } from "next-auth";
+import { LoginSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
+import bcrypt from "bcryptjs";
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      // TODO: Handle this error
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields?.success) {
+          const { email, password } = validatedFields?.data;
+
+          const user = await getUserByEmail(email);
+
+          if (!user || !user?.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user?.password);
+
+          if (passwordsMatch) return user;
+
+          return null;
+        }
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
 
 // Reference: https://authjs.dev/guides/upgrade-to-v5#edge-compatibility
