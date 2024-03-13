3f814ae64c4a5c461b0818c985f35446d7ac2211
chinmaykd21 <chinmaykd21@gmail.com>
2024-03-12 20:10:30 -0400

    seed a test user in local database for local development

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient, User } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+
+/**
+ * A global prisma client. It will be disconnecte dregardless of whether
+ * seed script has ran successfully OR unsucessfully.
+ */
+const prisma = new PrismaClient();
+
+async function main() {
+  const testUser: User = {
+    id: faker.string.alphanumeric(),
+    name: faker.person.firstName(),
+    email: "testUser1@email.com",
+    emailVerified: faker.date.recent({
+      days: 100,
+      refDate: "2023-07-01T00:00:00.000Z",
+    }),
+    password: "$2a$10$gTO7o3cdoCNUjVCNnrTeKu7aN5HMJ53ebEIoYZn6qeQIGiww2rjAu",
+  };
+
+  const user = await prisma.user.create({
+    data: {
+      ...testUser,
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
